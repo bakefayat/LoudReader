75c58108955389f320b5aab4f3136bb7c382e650
ehsan <bakefayat98@gmail.com>
2021-06-24 13:53:46 +0430

    code get cleared nothing functional got changed

--- combine.py
+++ combine.py
@@ -1,33 +1,20 @@
-import time
-from pygame import mixer
-from pydub import AudioSegment
-# from mutagen.mp3 import MP3
-import soundfile as sf
 import os
-from playsound import playsound as play
 import pandas as pd
+from pydub import AudioSegment
 
 #just play sound.
 def play_song(song, next = 0):
     path = r'C:\Users\RDaneshjoo\Downloads\programming\other projects\goya\voice\wav'
+    
     if (type(song) == int) and next == 1:
         second_part_of_path = str(song) + 'o' + '.wav'
-        fpath = os.path.join(path, second_part_of_path)
-        sleep_time = music_length(fpath)
-        sleep_time += 0.40
     elif (type(song) == int) and next == 0:
         second_part_of_path = str(song) + '.wav'
-        fpath = os.path.join(path, second_part_of_path)
-        sleep_time = music_length(fpath)
-        sleep_time += 0.40
     else:
         second_part_of_path = song + '.wav'
-        fpath = os.path.join(path, second_part_of_path)
-        sleep_time = music_length(fpath)
-        sleep_time += 0.5
-    mixer.Channel(i).play(mixer.Sound(fpath))
+
+    fpath = os.path.join(path, second_part_of_path)
     song_array.append(fpath)
-    time.sleep(sleep_time)
 # play 10-20-30-...
 def play_dah(num,next = 0):
     if num == 1:
@@ -78,14 +65,6 @@ def play_eleven_to_nineteen(num):
         play_song(18)
     if num == 9:
         play_song(19)
-
-#leave gap between them.
-def music_length(path):
-    # song = MP3(path)
-    song = sf.SoundFile(path)
-    song_length = len(path) / song.samplerate
-    # songl = song.info.length
-    return song_length
 
 #play first digit.
 def yekan(num, next = 0):
@@ -169,7 +148,6 @@ def split_num(number):
             yekan(yek,1)
         else:
             yekan(yek,0)
-    print('oh yellow')
     if mmz and mmz != '0':
         mmz = int(mmz)
         if not(yek) and not(dah):
@@ -180,10 +158,7 @@ def split_num(number):
             yekan(mmz)
         if not(yek) and not(dah):
             play_song('mil')
-    print('hi')
 i = 0
-mixer.init()
-mixer.music.set_volume(1)
 pan = pd.read_excel(r'C:\Users\RDaneshjoo\Desktop\master.xlsx')
 df = pd.DataFrame(pan, columns= [ 'طول','عرض','تعداد'])
 width = df['طول'].tolist()
@@ -197,10 +172,9 @@ for k in range(0,len(width)):
         split_num(width[k])
         play_dar()
         split_num(length[k])
-        time.sleep(0.3)
+        # time.sleep(0.3)
         split_num(many[k])
         play_adad()
-        print(song_array)
 for song in song_array:
     sound = AudioSegment.from_wav(song)
     combined += sound
