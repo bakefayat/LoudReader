38c8bad32f30dcd726ee10a1b63f47f624acf260
ehsan <bakefayat98@gmail.com>
2021-06-24 13:37:13 +0430

    program build wav file and its ok but need to remove additional modules and unused parts of code

--- combine.py
+++ combine.py
@@ -1,5 +1,6 @@
 import time
 from pygame import mixer
+from pydub import AudioSegment
 # from mutagen.mp3 import MP3
 import soundfile as sf
 import os
@@ -13,37 +14,70 @@ def play_song(song, next = 0):
         second_part_of_path = str(song) + 'o' + '.wav'
         fpath = os.path.join(path, second_part_of_path)
         sleep_time = music_length(fpath)
-        print(fpath)
         sleep_time += 0.40
     elif (type(song) == int) and next == 0:
         second_part_of_path = str(song) + '.wav'
         fpath = os.path.join(path, second_part_of_path)
         sleep_time = music_length(fpath)
-        print(fpath)
         sleep_time += 0.40
     else:
         second_part_of_path = song + '.wav'
         fpath = os.path.join(path, second_part_of_path)
         sleep_time = music_length(fpath)
-        print(fpath)
         sleep_time += 0.5
     mixer.Channel(i).play(mixer.Sound(fpath))
+    song_array.append(fpath)
     time.sleep(sleep_time)
 # play 10-20-30-...
 def play_dah(num,next = 0):
-    play_song(num*10)
+    if num == 1:
+        play_song(10, next)
+    if num == 2:
+        play_song(20, next)
+    if num == 3:
+        play_song(30, next)
+    if num == 4:
+        play_song(40, next)
+    if num == 5:
+        play_song(50, next)
+    if num == 6:
+        play_song(60, next)
+    if num == 7:
+        play_song(70, next)
+    if num == 8:
+        play_song(80, next)
+    if num == 9:
+        play_song(90, next)
 
 #play 100-200-300
 def play_sadgan(num, next = 0):
-    play_song(num*100)
+    if num == 1:
+        play_song(100, next)
+    if num == 2:
+        play_song(200, next)
+    if num == 3:
+        play_song(300, next)
 
 # play 11-19
 def play_eleven_to_nineteen(num):
-    num = str(num)
-    prefix = '1'
-    prefix += num
-    num = int(prefix)
-    play_song(num)
+    if num == 1:
+        play_song(11)
+    if num == 2:
+        play_song(12)
+    if num == 3:
+        play_song(13)
+    if num == 4:
+        play_song(14)
+    if num == 5:
+        play_song(15)
+    if num == 6:
+        play_song(16)
+    if num == 7:
+        play_song(17)
+    if num == 8:
+        play_song(18)
+    if num == 9:
+        play_song(19)
 
 #leave gap between them.
 def music_length(path):
@@ -55,7 +89,24 @@ def music_length(path):
 
 #play first digit.
 def yekan(num, next = 0):
-    play_song(num)
+    if num == 1:
+        play_song(1, next)
+    if num == 2:
+        play_song(2, next)
+    if num == 3:
+        play_song(3, next)
+    if num == 4:
+        play_song(4, next)
+    if num == 5:
+        play_song(5, next)
+    if num == 6:
+        play_song(6, next)
+    if num == 7:
+        play_song(7, next)
+    if num == 8:
+        play_song(8, next)
+    if num == 9:
+        play_song(9, next)
 
 #play second digit
 def dahgan(num, yekan, mmz = 0, next= 0):
@@ -80,10 +131,9 @@ def play_adad():
         pass
 #play milimeter after length or width.
 def play_mil():
-        play_song('mil')
-        # pass
+        # play_song('mil')
+        pass
 def split_num(number):
-    number2 = number
     mmz_index = None
     mmz = 0
     sad = 0
@@ -119,6 +169,7 @@ def split_num(number):
             yekan(yek,1)
         else:
             yekan(yek,0)
+    print('oh yellow')
     if mmz and mmz != '0':
         mmz = int(mmz)
         if not(yek) and not(dah):
@@ -129,6 +180,7 @@ def split_num(number):
             yekan(mmz)
         if not(yek) and not(dah):
             play_song('mil')
+    print('hi')
 i = 0
 mixer.init()
 mixer.music.set_volume(1)
@@ -137,6 +189,10 @@ df = pd.DataFrame(pan, columns= [ 'طول','عرض','تعداد'])
 width = df['طول'].tolist()
 length = df['عرض'].tolist()
 many = df['تعداد'].tolist()
+song_array = []
+file1 = r'C:\Users\RDaneshjoo\Downloads\programming\other projects\goya\voice\wav\blank.wav'
+sound1 = AudioSegment.from_wav(file1)
+combined = sound1
 for k in range(0,len(width)):
         split_num(width[k])
         play_dar()
@@ -144,3 +200,8 @@ for k in range(0,len(width)):
         time.sleep(0.3)
         split_num(many[k])
         play_adad()
+        print(song_array)
+for song in song_array:
+    sound = AudioSegment.from_wav(song)
+    combined += sound
+combined.export("joinedFile.wav", format="wav")
